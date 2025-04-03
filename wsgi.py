96393518487ab20c69479efa8e4b7cd491c960b3
marguberk <your.email@example.com>
2025-04-03 08:01:45 +0500

    Updated configuration for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,23 @@
-import sys
 import os
+import sys
 
-# Добавляем путь к приложению в системные пути
-path = '/home/YOUR_PYTHONANYWHERE_USERNAME/skillsoft'
+# Путь к директории с приложением
+path = '/home/marguberk/skillsoft'
 if path not in sys.path:
     sys.path.append(path)
+
+# Активируем виртуальное окружение
+VIRTUALENV = '/home/marguberk/.virtualenvs/skillsoft-env'
+if os.path.exists(VIRTUALENV):
+    activate_this = os.path.join(VIRTUALENV, 'bin/activate_this.py')
+    if os.path.exists(activate_this):
+        with open(activate_this) as file_:
+            exec(file_.read(), dict(__file__=activate_this))
+
+# Устанавливаем переменные окружения
+os.environ['FLASK_APP'] = 'app'
+os.environ['FLASK_ENV'] = 'production'
+os.environ.setdefault('FLASK_SETTINGS_MODULE', 'config.ProductionConfig')
 
 # Импортируем и создаем приложение Flask
 from app import create_app
